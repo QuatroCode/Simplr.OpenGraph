9e6e003a073b7b391db55117440850c7bcf76a50
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 20:00:57 +0200

    Added method which extract KeyValue from properties

--- Helpers/OGHelper.cs
+++ Helpers/OGHelper.cs
@@ -1,9 +1,76 @@
-﻿using System.Text;
+﻿using Simplr.OpenGraph.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
 
 namespace Simplr.OpenGraph.Helpers
 {
     public static class OGHelper
     {
+        private static IList<OGKeyValue> ExtractKeyValueFromProperties(IEnumerable<PropertyInfo> properties, object valueObject, string originalName)
+        {
+            var result = new List<OGKeyValue>();
+            foreach (var property in properties)
+            {
+                var separator = ":";
+                var name = property.Name.AddSymbolBeforeUpperCase('_').ToLower();
+                var value = property.GetValue(valueObject);
+                if (value == null || value as int? == 0)
+                {
+                    continue;
+                }
+                if (name == "type")
+                {
+                    continue;
+                }
+                if (name == "url")
+                {
+                    separator = "";
+                    name = "";
+                }
+
+                var valueType = value.GetType();
+                if (valueType.Name != "String" && valueType.Name != "Uri" && valueType.Name != "Int32" && valueType.Name != "DateTime")
+                {
+                    if (valueType.Name == "List`1")
+                    {
+                        foreach (var item in value as IList)
+                        {
+                            var itemType = item.GetType();
+                            if (itemType.Name != "String")
+                            {
+                                var itemBaseType = itemType.GetTypeInfo().BaseType;
+                                result.AddRange(ExtractKeyValueFromProperties(itemBaseType.GetRuntimeProperties(), item, name.ToLower()));
+
+                                result.AddRange(ExtractKeyValueFromProperties(itemType.GetTypeInfo().DeclaredProperties, item, name.ToLower()));
+                            }
+                            else
+                            {
+                                result.Add(new OGKeyValue()
+                                {
+                                    Name = name.AddSymbolBeforeUpperCase(':').ToLower(),
+                                    Content = item.ToString()
+                                });
+                            }
+                        }
+                        continue;
+                    }
+                    result.AddRange(ExtractKeyValueFromProperties(valueType.GetTypeInfo().DeclaredProperties, value, name.ToLower()));
+                    continue;
+                }
+                else
+                {
+                    result.Add(new OGKeyValue()
+                    {
+                        Name = string.Format("{0}{1}{2}", originalName, separator, name),
+                        Content = value.ToString()
+                    });
+                }
+            }
+            return result;
+        }
+
         public static string AddDotInsteadOfUnderscoreAndAddUnderscoreBeforeUpperCase(string enumValue)
         {
             var changeUnderscoreToDot = enumValue.AddDotInsteadOfUnderscore();
