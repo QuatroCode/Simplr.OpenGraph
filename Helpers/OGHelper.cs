450da20ce4227a928db2f78a8424ea7cc9893d24
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:54:50 +0200

    Added method which add symbol before upperCase

--- Helpers/OGHelper.cs
+++ Helpers/OGHelper.cs
@@ -4,6 +4,24 @@ namespace Simplr.OpenGraph.Helpers
 {
     public static class OGHelper
     {
+        private static string AddSymbolBeforeUpperCase(this string text, char symbol)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+            StringBuilder newText = new StringBuilder(text.Length * 2);
+            newText.Append(text[0]);
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]) && text[i - 1] != '.')
+                {
+                    newText.Append(symbol);
+                }
+                newText.Append(text[i]);
+            }
+            return newText.ToString();
+        }
         private static string AddDotInsteadOfUnderscore(this string text)
         {
             if (string.IsNullOrWhiteSpace(text))
