70ad5f44c02fd8ff31f7f07f631b325584ff94df
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:48:04 +0200

    Added OGAlbum model

---
+++ Models/Music/OGAlbum.cs
@@ -0,0 +1,24 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Simplr.OpenGraph.Models.Music
+{
+    public class OGAlbum : IOGTypeInternal
+    {
+        public OGObjectType Type { get { return OGObjectType.Music_Album; } }
+        /// <summary>
+        /// The song on this album.
+        /// </summary>
+        public List<OGMusicSong> Song { get; set; }
+        /// <summary>
+        /// The musician that made this song.
+        /// </summary>
+        public OGProfile Musician { get; set; }
+        /// <summary>
+        /// The date the album was released.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
