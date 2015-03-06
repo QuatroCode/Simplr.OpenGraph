90047418651dc08e64ade02cbfa0f274626942ff
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:32:48 +0200

    Added OGObjectType enum

---
+++ Enums/OGObjectType.cs
@@ -0,0 +1,18 @@
+﻿namespace Simplr.OpenGraph.Enums
+{
+    public enum OGObjectType
+    {
+        Music_Song,
+        Music_Album,
+        Music_Playlist,
+        Music_RadioStation,
+        Video_Movie,
+        Video_Episode,
+        Video_TvShow,
+        Video_Other,
+        Article,
+        Book,
+        Profile,
+        Website
+    }
+}
