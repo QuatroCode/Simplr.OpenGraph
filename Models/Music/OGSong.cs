6cf13bbfaf021797e650bcd98aadae7f3eca7baf
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:49:07 +0200

    Added OGSong model

---
+++ Models/Music/OGSong.cs
@@ -0,0 +1,23 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+using System.Collections.Generic;
+
+namespace Simplr.OpenGraph.Models.Music
+{
+    public class OGSong : IOGTypeInternal
+    {
+        public OGObjectType Type { get { return OGObjectType.Music_Song; } }
+        /// <summary>
+        /// >=1 - The song's length in seconds.
+        /// </summary>
+        public int Duration { get; set; }
+        /// <summary>
+        /// The album this song is from.
+        /// </summary>
+        public List<OGMusicAlbum> Album { get; set; }
+        /// <summary>
+        /// The musician that made this song.
+        /// </summary>
+        public List<OGProfile> Musician { get; set; }
+    }
+}
