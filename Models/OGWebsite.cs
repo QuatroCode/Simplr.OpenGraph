c366bca23121f0d05bece7d7697103a3b1585e2e
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:41:55 +0200

    Added OGWebsite model

---
+++ Models/OGWebsite.cs
@@ -0,0 +1,10 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+
+namespace Simplr.OpenGraph.Models
+{
+    public class OGWebsite : IOGTypeInternal
+    {
+        public OGObjectType Type { get { return OGObjectType.Website; } }
+    }
+}
