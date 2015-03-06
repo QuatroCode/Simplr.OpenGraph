34c820d581ceb2655040eac6d16f5e69e7b9a00b
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:43:56 +0200

    Added OGImage model

---
+++ Models/OGImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Simplr.OpenGraph.Models
+{
+    public class OGImage
+    {
+        /// <summary>
+        /// An image URL which should represent your object within the graph.
+        /// </summary>
+        public Uri Url { get; set; }
+        /// <summary>
+        /// An alternate url to use if the webpage requires HTTPS.
+        /// </summary>
+        public Uri SecureUrl { get; set; }
+        /// <summary>
+        /// A MIME type for this image. E.g. http://en.wikipedia.org/wiki/Internet_media_type
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// The number of pixels wide.
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// The number of pixels high.
+        /// </summary>
+        public int Height { get; set; }
+
+        public OGImage()
+        {
+
+        }
+        public OGImage(Uri uri)
+        {
+            Url = uri;
+        }
+        public OGImage(Uri uri, Uri secureUrl, string type, int width, int height)
+        {
+            Url = uri;
+            SecureUrl = secureUrl;
+            Type = type;
+            Width = width;
+            Height = height;
+        }
+    }
+}
