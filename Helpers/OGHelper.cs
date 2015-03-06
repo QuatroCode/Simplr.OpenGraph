221300a854e0a359667de8e7f5690144a104ccaf
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 20:05:06 +0200

    Added method which get KeyValue

--- Helpers/OGHelper.cs
+++ Helpers/OGHelper.cs
@@ -1,4 +1,6 @@
-﻿using Simplr.OpenGraph.Models;
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+using Simplr.OpenGraph.Models;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -8,6 +10,86 @@ namespace Simplr.OpenGraph.Helpers
 {
     public static class OGHelper
     {
+        private static IList<OGKeyValue> GetKeyValue<T>(IOGType<T> data)
+        {
+            var result = new List<OGKeyValue>();
+            var baseType = data.GetType().GetTypeInfo().BaseType;
+            var properties = baseType.GetRuntimeProperties();
+
+            if (baseType == typeof(object))
+            {
+                properties = data.GetType().GetTypeInfo().DeclaredProperties;
+            }
+            foreach (var property in properties)
+            {
+                var name = property.Name;
+                var value = property.GetValue(data);
+
+                if (value == null)
+                {
+                    continue;
+                }
+
+                var valueType = value.GetType();
+                if (valueType.Name != "String" && valueType.Name != "Uri")
+                {
+                    if (valueType.Name == "List`1")
+                    {
+                        foreach (var item in value as IList)
+                        {
+                            var itemType = item.GetType();
+                            if (itemType.Name != "String")
+                            {
+                                var itemBaseType = itemType.GetTypeInfo().BaseType;
+                                result.AddRange(ExtractKeyValueFromProperties(itemBaseType.GetRuntimeProperties(), item, name.ToLower()));
+
+                                result.AddRange(ExtractKeyValueFromProperties(itemType.GetTypeInfo().DeclaredProperties, item, name.ToLower()));
+                            }
+                            else
+                            {
+                                result.Add(new OGKeyValue()
+                                {
+                                    Name = name.AddSymbolBeforeUpperCase(':').ToLower(),
+                                    Content = item.ToString()
+                                });
+                            }
+                        }
+                        continue;
+                    }
+                    if (valueType == typeof(OGDeterminer))
+                    {
+                        var enumValue = value.ToString();
+                        if (OGDeterminer.Blank == value as OGDeterminer?)
+                        {
+                            enumValue = "";
+                        }
+                        result.Add(new OGKeyValue()
+                        {
+                            Name = name.ToLower(),
+                            Content = enumValue.ToLower()
+                        });
+                        continue;
+                    }
+                    if (name == "Type")
+                    {
+                        result.Add(new OGKeyValue()
+                        {
+                            Name = name.ToLower(),
+                            Content = AddDotInsteadOfUnderscoreAndAddUnderscoreBeforeUpperCase(value.ToString())
+                        });
+                        continue;
+                    }
+                    result.AddRange(ExtractKeyValueFromProperties(valueType.GetTypeInfo().DeclaredProperties, value, name.ToLower()));
+                    continue;
+                }
+                result.Add(new OGKeyValue()
+                {
+                    Name = name.AddSymbolBeforeUpperCase('_').ToLower(),
+                    Content = value.ToString()
+                });
+            }
+            return result;
+        }
         private static string SetMetaProperties(IList<OGKeyValue> data)
         {
             string result = null;
