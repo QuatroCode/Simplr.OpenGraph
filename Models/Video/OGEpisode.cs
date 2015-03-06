32f1b7ff871da4aec68789cb5b80fb47958b6a87
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:47:10 +0200

    Added OGEpisode model

---
+++ Models/Video/OGEpisode.cs
@@ -0,0 +1,14 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+
+namespace Simplr.OpenGraph.Models.Video
+{
+    public class OGEpisode : OGMovie, IOGTypeInternal
+    {
+        public override OGObjectType Type { get { return OGObjectType.Video_Episode; } }
+        /// <summary>
+        /// Which series this episode belongs to.
+        /// </summary>
+        public OGTvShow Series { get; set; }
+    }
+}
