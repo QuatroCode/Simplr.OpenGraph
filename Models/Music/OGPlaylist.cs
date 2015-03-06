7d15fe878169d90487a1e3cdcfe99839248cf1b0
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:50:16 +0200

    Added OGPlaylist model

---
+++ Models/Music/OGPlaylist.cs
@@ -0,0 +1,19 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+using System.Collections.Generic;
+
+namespace Simplr.OpenGraph.Models.Music
+{
+    public class OGPlaylist : IOGTypeInternal
+    {
+        public OGObjectType Type { get { return OGObjectType.Music_Playlist; } }
+        /// <summary>
+        /// The songs on this playlist.
+        /// </summary>
+        public List<OGMusicSong> Song { get; set; }
+        /// <summary>
+        /// The creator of this playlist.
+        /// </summary>
+        public OGProfile Creator { get; set; }
+    }
+}
