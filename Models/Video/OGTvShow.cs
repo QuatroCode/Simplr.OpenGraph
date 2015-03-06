9ebefce8a0b25c0e1e06a207409140eef8bfe6c0
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:46:02 +0200

    Added OGTvShow model

---
+++ Models/Video/OGTvShow.cs
@@ -0,0 +1,10 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+
+namespace Simplr.OpenGraph.Models.Video
+{
+    public class OGTvShow : OGMovie, IOGTypeInternal
+    {
+        public override OGObjectType Type { get { return OGObjectType.Video_Movie; } }
+    }
+}
