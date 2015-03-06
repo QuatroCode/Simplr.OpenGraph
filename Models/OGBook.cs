d8858801bae75ee475b61632ae4f8739f3e586c9
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:44:23 +0200

    Added OGBook model

---
+++ Models/OGBook.cs
@@ -0,0 +1,28 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Simplr.OpenGraph.Models
+{
+    public class OGBook : IOGTypeInternal
+    {
+        public OGObjectType Type { get { return OGObjectType.Book; } }
+        /// <summary>
+        /// Who wrote this book.
+        /// </summary>
+        public List<OGProfile> Author { get; set; }
+        /// <summary>
+        /// The ISBN (International Standard Book Number)
+        /// </summary>
+        public string Isbn { get; set; }
+        /// <summary>
+        /// The date the book was released.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+        /// <summary>
+        /// Tag words associated with this book.
+        /// </summary>
+        public List<string> Tag { get; set; }
+    }
+}
