477e3481b45140025594133be1133522f9f4a5a1
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:46:21 +0200

    Added OGOther model

---
+++ Models/Video/OGOther.cs
@@ -0,0 +1,10 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+
+namespace Simplr.OpenGraph.Models.Video
+{
+    public class OGOther : OGMovie, IOGTypeInternal
+    {
+        public override OGObjectType Type { get { return OGObjectType.Video_Other; } }
+    }
+}
