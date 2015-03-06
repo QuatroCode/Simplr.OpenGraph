c89248186409a43fdb97b928e97569d0ec0e5206
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:49:58 +0200

    Added OGRadioStation model

---
+++ Models/Music/OGRadioStation.cs
@@ -0,0 +1,14 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+
+namespace Simplr.OpenGraph.Models.Music
+{
+    public class OGRadioStation : IOGTypeInternal
+    {
+        public OGObjectType Type { get { return OGObjectType.Music_RadioStation; } }
+        /// <summary>
+        /// The creator of this station.
+        /// </summary>
+        public OGProfile Creator { get; set; }
+    }
+}
