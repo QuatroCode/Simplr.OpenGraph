59cb65339818b20093b7ac83908d57ba65428b5a
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:49:31 +0200

    Added OGMusicSong model

---
+++ Models/Music/OGMusicSong.cs
@@ -0,0 +1,14 @@
+﻿namespace Simplr.OpenGraph.Models.Music
+{
+    public class OGMusicSong : OGSong
+    {
+        /// <summary>
+        /// >=1 - Which disc of the album this song is on.
+        /// </summary>
+        public int Disc { get; set; }
+        /// <summary>
+        /// >=1 - Which track this song is.
+        /// </summary>
+        public int Track { get; set; }
+    }
+}
