d823a09dbc6b6fbae296ff5c96109cc2d8f57c38
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:46:46 +0200

    Added OGMovie model

---
+++ Models/Video/OGMovie.cs
@@ -0,0 +1,36 @@
+﻿using Simplr.OpenGraph.Contracts;
+using Simplr.OpenGraph.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Simplr.OpenGraph.Models.Video
+{
+    public class OGMovie : IOGTypeInternal
+    {
+        public virtual OGObjectType Type { get { return OGObjectType.Video_Movie; } }
+        /// <summary>
+        /// Actors in the movie.
+        /// </summary>
+        public List<OGActor> Actor { get; set; }
+        /// <summary>
+        /// Directors of the movie.
+        /// </summary>
+        public List<OGProfile> Director { get; set; }
+        /// <summary>
+        /// Writers of the movie.
+        /// </summary>
+        public List<OGProfile> Writer { get; set; }
+        /// <summary>
+        /// >=1 - The movie's length in seconds.
+        /// </summary>
+        public int Duration { get; set; }
+        /// <summary>
+        /// The date the movie was released.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+        /// <summary>
+        /// Tag words associated with this movie.
+        /// </summary>
+        public List<string> Tag { get; set; }
+    }
+}
