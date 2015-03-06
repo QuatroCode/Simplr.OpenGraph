6a3d8d5f04aba6227f2d46a6ecb3a2f941fa2c76
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:47:33 +0200

    Added OGActor model

---
+++ Models/Video/OGActor.cs
@@ -0,0 +1,10 @@
+﻿namespace Simplr.OpenGraph.Models.Video
+{
+    public class OGActor : OGProfile
+    {
+        /// <summary>
+        /// The role they played.
+        /// </summary>
+        public string Role { get; set; }
+    }
+}
