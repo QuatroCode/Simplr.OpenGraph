0314c1da27f778c5febd8ebd148b47eda1f7d576
Donatas Brazionis <brazdonatas@gmail.com>
2015-03-06 19:48:29 +0200

    Added OGMusicAlbum model

---
+++ Models/Music/OGMusicAlbum.cs
@@ -0,0 +1,14 @@
+﻿namespace Simplr.OpenGraph.Models.Music
+{
+    public class OGMusicAlbum : OGAlbum
+    {
+        /// <summary>
+        /// >=1 - Which disc of the song this album is on.
+        /// </summary>
+        public int Disc { get; set; }
+        /// <summary>
+        /// >=1 - Which track this album is.
+        /// </summary>
+        public int Track { get; set; }
+    }
+}
